ff7e748af178d37090adde1786023a92a3745624
shu <shu@shu.moe>
2021-12-09 17:39:03 +0800

    feat: resize editor when partner change size

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -6,16 +6,29 @@ export function Editor() {
   const divEl = useRef<HTMLDivElement>(null)
   let editor: monaco.editor.IStandaloneCodeEditor
   useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      editor?.layout()
+    })
     if (divEl.current) {
       editor = monaco.editor.create(divEl.current, {
         value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
           '\n'
         ),
         language: 'typescript',
+        fontSize: 16,
+        tabSize: 2,
+        fontLigatures: true,
+        renderWhitespace: 'boundary',
+        wordWrap: 'on',
+        renderControlCharacters: true,
+        rulers: [80, 120],
+        theme: 'vs-light+'
       })
+      resizeObserver.observe(divEl.current)
     }
     return () => {
       editor.dispose()
+      resizeObserver.disconnect()
     }
   }, [])
   return <div className={styles.editor} ref={divEl}></div>
